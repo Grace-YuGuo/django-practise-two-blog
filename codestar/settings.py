5ef21079a2eb0745f8dad651532416456a908eca
Yu Guo <grace.guo012@gmail.com>
2024-07-19 12:46:47 +0000

    With tutor's support,changed database link url

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=*hjq65z4oki-9eax6x1-ag@b3u#q)gur=it=q6bs198-xb#ju'
+SECRET_KEY =  os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -84,6 +84,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
 DATABASES={
     'default':dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
