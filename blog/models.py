273cd6500df47c83203484f752f436755bb832bf
Yu Guo <grace.guo012@gmail.com>
2024-07-23 13:52:42 +0000

    Add comment by admin

--- blog/models.py
+++ blog/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name= "commnets")
+    post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name= "comments")
     author = models.ForeignKey(User, on_delete = models.CASCADE, related_name="commenter")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add= True)
