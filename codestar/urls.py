647073ada76f464aa47c463b52e5ec354e2ebbc6
Yu Guo <grace.guo012@gmail.com>
2024-07-23 12:27:21 +0000

    Add authentication

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,6 +20,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('about/', include('about.urls')),
+    path("accouts/",include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls'), name='blog-urls'),
