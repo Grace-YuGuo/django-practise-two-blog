4c8c5ebe13492f93b54108c0d09443e12d8c68fc
Yu Guo <grace.guo012@gmail.com>
2024-07-24 09:24:06 +0000

    Add collaborate form to submit and check in admin page

--- about/views.py
+++ about/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from django.views import generic
+from django.contrib import messages
 from .models import About
 from django.shortcuts import render,get_object_or_404
+from .forms import CollaborateRequestForm
 
 # Create your views here.
 def about(request):
@@ -17,6 +19,17 @@ def about(request):
     :template:`about/About.html`
     """
     about = About.objects.all().order_by('-created_on').first()
+    # Handle the POST request from the comment form
+    if request.method == "POST":
+         collaborator_form = CollaborateRequestForm(data=request.POST)
+         print(collaborator_form)
+         if collaborator_form.is_valid():
+            collaborate = collaborator_form.save(commit=False)
+            collaborate.read=False
+            collaborate.save()
+            messages.add_message(request,messages.SUCCESS,'Collaboration request received! I endeavour to respond within 2 working days.')
 
-    return render(request,"about/about.html",{"about":about,},
+    collaborator_form = CollaborateRequestForm()
+    return render(request,"about/about.html",{"about":about,
+    'collaborator_request_form':collaborator_form,}
     )
