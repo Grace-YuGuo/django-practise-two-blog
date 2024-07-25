f28cecc94a4f61b428b24b1a5b3f0ac59db22d28
Yu Guo <grace.guo012@gmail.com>
2024-07-25 10:56:29 +0000

    Add test case for blog form and about form

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -3,11 +3,14 @@ from .forms import CommentForm
 
 
 class TestCommentForm(TestCase):
+    """
+    Test the comment form in blog app
+    """
 
     def test_form_is_valid(self):
-        comment_form = CommentForm({'body': 'The'})
-        self.assertTrue(comment_form.is_valid())
+        comment_form = CommentForm({'body': 'The blog content is A'})
+        self.assertTrue(comment_form.is_valid(), msg='Blog form is not valid')
 
-    def test_form_is_invalid(self):
-        comment_form = CommentForm({'body': ''})
-        self.assertFalse(comment_form.is_valid(), msg='Form is valid')
+#     def test_form_is_invalid(self):
+#         comment_form = CommentForm({'body': ''})
+#         self.assertFalse(comment_form.is_valid(), msg='Form is valid')
